fe805fca8c5eb65a8a4a7c770b2a8bfbbb383b88
Stanislav <stanislav.dinev.nine@gmail.com>
2020-10-30 13:45:54 +0200

    s

--- mycv/settings.py
+++ mycv/settings.py
@@ -123,3 +123,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'mycv', 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
