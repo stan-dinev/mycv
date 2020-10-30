506b0212f8bb110429b8d898af75576f48267d90
Stanislav <stanislav.dinev.nine@gmail.com>
2020-10-30 16:18:14 +0200

    s

--- mycv/settings.py
+++ mycv/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'mycv', 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
